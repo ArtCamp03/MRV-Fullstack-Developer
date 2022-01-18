49e41c202c5a64fe446ee49a4f75ceee139d7911
Artur R Campos <arturrc10@outlook.com>
2022-01-18 19:33:00 -0300

    solving exercises

---
+++ Programando com JavaScript - Iniciante/Exercicio3/index.js
@@ -0,0 +1,12 @@
+let lines = gets().split('\n')
+
+let N = parseInt(lines.shift());
+
+for(let i = 0; i <= N; i++) {
+    if(N => 1 && N <= 1000){
+        if(i % 2 != 0){
+            print(i,'\n');
+        }
+    }
+    
+}
