76fcf211bc7033c6564b37f273fdaf6b008a782b
Artur R Campos <arturrc10@outlook.com>
2022-01-20 14:23:27 -0300

    iffod interface in react Native

---
+++ Criando a interface do iFood com React Native/projeto/src/components/CategoriaItem/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const CategoriaView = styled.view`
+
+        margin-left: 20px;
+        align-items: center;
+
+`
+
+export const CategoriaFoto = styled.image`
+
+`
+
+export const CategoriaTexto = styled.text`
+
+    margin-top:5px;
+
+`
