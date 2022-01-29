38fbfea4f01c8acf04631a6f4de7aecc798e71fb
Artur R Campos <arturrc10@outlook.com>
2022-01-29 00:30:36 -0300

    project one

---
+++ Programando com Orientação a Objeto com C#/abstraçao/exemploPOO/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace exemploPOO.Models
+{
+    public class Pessoa
+    {
+        public string Nome{get; set;}
+        public int Idade{get; set;}
+
+        public void Apresentar(){
+            Console.WriteLine($"Ola meu nome e {Nome} e tenho {Idade} anos");
+        }
+    }
+}
--- Programando com Orientação a Objeto com C#/abstraçao/exemploPOO/Program.cs
+++ Programando com Orientação a Objeto com C#/abstraçao/exemploPOO/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using exemploPOO.Models;
+
+namespace exemploPOO{
+    class Program{
+        static void Main(string[] args){
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Joao";
+            p1.Idade = 27;
+
+            p1.Apresentar();
+        }
+    }
+}
