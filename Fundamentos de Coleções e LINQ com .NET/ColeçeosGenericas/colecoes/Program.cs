10f5f22a0490b7025c45ab0b1cea9d56f342bc47
Artur R Campos <arturrc10@outlook.com>
2022-02-10 22:47:28 -0300

    att

--- Fundamentos de Coleções e LINQ com .NET/ColeçeosGenericas/colecoes/Program.cs
+++ Fundamentos de Coleções e LINQ com .NET/ColeçeosGenericas/colecoes/Program.cs
@@ -3,28 +3,57 @@
 
         static void Main(string[] args){
 
-            OperacaoLista opLista = new OperacaoLista();
-            string[] estadoArrays = new string[2] {"GO","RS"};
+            Stack<string> pilhaLivros = new Stack<string>;
 
-            List<string> estados = new List<string> {"BA", "PA"};
+            pilhaLivros.Push(".NET");
+            pilhaLivros.Push("DDD");
+            pilhaLivros.Push("Codigo Limpo");
+
+            System.Console.WriteLine($"Livros para leitura: {pilhaLivros.Count}");
+            while(pilhaLivros.Count > 0){
+                System.Console.WriteLine($"Proximo livro para a aleitura: {pilhaLivros.Peek()}");
+                System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso");
+            }
+
+            System.Console.WriteLine($"Livros para leitura: {pilhaLivros.Count}");
+
+            // Queue<string> fila = new Queue<string>;
+
+            // fila.Enqueue("Joao");
+            // fila.Enqueue("Marcos");
+            // fila.Enqueue("Lucas");
+
+            // System.Console.WriteLine($"Pessoas na fila: {fila.Count()}");
+
+            // while(fila.Count > 0){
+            //     System.Console.WriteLine($"Vex de: {fila.Peek()}");
+            //     System.Console.WriteLine($" {fila.Dequeue()} atendido");
+            // }
+
+            // System.Console.WriteLine($"Pessoas na fila: {fila.Count()}");
+
+            // OperacaoLista opLista = new OperacaoLista();
+            // string[] estadoArrays = new string[2] {"GO","RS"};
+
+            // List<string> estados = new List<string> {"BA", "PA"};
 
             // estados.Add("MT");
             // estados.Add("GO");
             // estados.Add("MS");
             // estados.Add("SP");
 
-            System.Console.WriteLine($"quantidades de elementos na lista: {estados.Count}");
+            // System.Console.WriteLine($"quantidades de elementos na lista: {estados.Count}");
 
-            opLista.ImprimirListaString(estados);
+            // opLista.ImprimirListaString(estados);
 
             // System.Console.WriteLine("Removendo o elemento");
             // estados.Remove("MG");
 
-            estados.AddRange(estadoArrays);
+            // estados.AddRange(estadoArrays);
 
-            estados.Insert(1, "RJ");
+            // estados.Insert(1, "RJ");
 
-            opLista.ImprimirListaString(estados);
+            // opLista.ImprimirListaString(estados);
 
             // foreach (var item in estados){
             //     System.Console.WriteLine(item);
